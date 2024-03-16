97b2bdadf7412f7362d4900e904074bd7cb0b32f
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2024-03-17 00:10:42 +0100

    (fix): Upgrade Tanstack to v5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { ReactQueryClientProvider } from '@/api/react-query-client-provider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,16 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <ReactQueryClientProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+          <ReactQueryDevtools />
+        </body>
+      </html>
+    </ReactQueryClientProvider>
   );
 }
---
+++ src/features/courses/api/use-get-courses.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useEffect } from 'react';
+import {
+  useQuery,
+  useQueryClient,
+  keepPreviousData,
+} from '@tanstack/react-query';
+import { getCourses } from '@/api/courses';
+import type { Page } from '@/typings/page.types';
+import type { Course } from '@/api/courses.types';
+
+const QUERY_KEY = 'courses';
+
+export function getQueryKey(page?: number) {
+  if (page === undefined) {
+    return [QUERY_KEY];
+  }
+  return [QUERY_KEY, page];
+}
+
+export function useGetCourses(page: number) {
+  const query = useQuery<Page<Course>, Error>({
+    queryKey: getQueryKey(page),
+    queryFn: ({ signal }) => getCourses(page, { signal }),
+    placeholderData: keepPreviousData,
+  });
+
+  // Prefetch next page
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    if (query.data?.meta.hasNextPage) {
+      queryClient.prefetchQuery({
+        queryKey: getQueryKey(page + 1),
+        queryFn: ({ signal }) => getCourses(page + 1, { signal }),
+      });
+    }
+  }, [query.data, page, queryClient]);
+  return query;
+}
