9ee14083df2d6e12008e5d84b29b49bb311484b8
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2024-03-19 23:14:35 +0100

    (feat): Added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className='py-3 items-center justify-center mt-auto shadow-sm border-t'>
+      <p className='text-white text-center'>© 2024 Piotr Pabich</p>
+    </footer>
+  );
+}
