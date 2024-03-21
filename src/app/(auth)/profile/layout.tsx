d310325df7d7c6034a3bdae0e259804fe4ea4843
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2024-03-21 23:58:33 +0100

    feat(profile): Added ProfilePage

---
+++ src/app/(auth)/profile/layout.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { Separator } from '@/components/ui/separator';
+import { SidebarNav } from './_components/sidebar-nav';
+import useAuthStore from '@/store/useAuthStore';
+
+const sidebarNavItems = [
+  {
+    title: 'Profile',
+    href: '/profile',
+  },
+  {
+    title: 'Account',
+    href: '/profile/account',
+  },
+  {
+    title: 'Appearance',
+    href: '/profile/appearance',
+  },
+  {
+    title: 'Courses',
+    href: '/courses',
+  },
+];
+
+interface ProfileLayoutSettings {
+  children: React.ReactNode;
+}
+
+export default function ProfileLayout({ children }: ProfileLayoutSettings) {
+  const user = useAuthStore().user;
+  return (
+    <>
+      <div className='space-y-6 p-10 pb-16 md:block'>
+        <div className='space-y-0.5'>
+          <h2 className='text-2xl font-bold tracking-tight'>{`👋 Hello ${user?.firstName}`}</h2>
+          <p className='text-muted-foreground'>
+            Manage your account settings in here
+          </p>
+        </div>
+        <Separator className='my-6' />
+        <div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
+          <aside className='-mx-4 lg:w-1/5'>
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className='flex-1 lg:max-w-2xl'>{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/(auth)/profile/profile-form.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { toast } from '@/components/ui/use-toast';
+import useAuthStore from '@/store/useAuthStore';
+
+const profileFormSchema = z.object({
+  username: z
+    .string()
+    .min(2, {
+      message: 'Username must be at least 2 characters.',
+    })
+    .max(30, {
+      message: 'Username must not be longer than 30 characters.',
+    }),
+  email: z
+    .string({
+      required_error: 'Please select an email to display.',
+    })
+    .email(),
+  password: z
+    .string()
+    .min(4, { message: 'Password must be at least 4 characters' }),
+  bio: z.string().max(160).min(4),
+});
+
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
+
+export function ProfileForm() {
+  const user = useAuthStore().user;
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues: {
+      bio: user?.bio,
+      email: user?.email,
+      username: user?.username,
+    },
+    mode: 'onChange',
+  });
+
+  function onSubmit(data: ProfileFormValues) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
+          <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input {...field} disabled />
+              </FormControl>
+              <FormDescription>
+                This is your public display name. It can be your real name or
+                nickname. This is also your login.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <Input {...field} />
+              <FormDescription>
+                This email address will be your primary contact. We will use it
+                to send notifications, confirmation emails and password change
+                requests.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='bio'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder='Tell us a little bit about yourself'
+                  className='resize-none'
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                You can <span>@mention</span> other users and organizations to
+                link to them.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Update profile</Button>
+      </form>
+    </Form>
+  );
+}
